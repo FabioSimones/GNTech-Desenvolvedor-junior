a83906dae062b4d31f56ba404644b5ac7c4177c4
FabioSimones <simones.fabio1994@gmail.com>
2026-01-07 18:57:14 -0300

    Criação da camada repository.

---
+++ src/main/java/com/devfabiosimones/api/repository/EnderecoRepository.java
@@ -0,0 +1,7 @@
+package com.devfabiosimones.api.repository;
+
+import com.devfabiosimones.api.entity.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+}
