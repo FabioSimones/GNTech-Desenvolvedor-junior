6952827dc95f91ed81d4b993d70bc29ab357bc64
FabioSimones <simones.fabio1994@gmail.com>
2026-01-07 18:39:11 -0300

    Projeto criado.

---
+++ src/main/java/com/devfabiosimones/api/DesafioGnTechApplication.java
@@ -0,0 +1,13 @@
+package com.devfabiosimones.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioGnTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioGnTechApplication.class, args);
+	}
+
+}
